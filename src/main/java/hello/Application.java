c73dfb418f28e4886daa3cc7e8009566369c2c1d
Héctor <hectorlopezfernandez@hotmail.com>
2015-12-18 23:33:07 +0100

    Minor fixes and application.properties

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,13 +2,14 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class Application {
 
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
