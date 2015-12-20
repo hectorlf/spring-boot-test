3f2362792a2be9fffb54f8590659c64825c02058
Héctor <hectorlopezfernandez@hotmail.com>
2015-12-20 21:38:36 +0100

    Configure undertow for ports 8080 and 8443 with a self-signed cert

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,8 +1,14 @@
 package hello;
 
+import io.undertow.Undertow.Builder;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.undertow.UndertowBuilderCustomizer;
+import org.springframework.boot.context.embedded.undertow.UndertowEmbeddedServletContainerFactory;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @ServletComponentScan
@@ -10,6 +16,19 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+    	UndertowEmbeddedServletContainerFactory containerFactory = new UndertowEmbeddedServletContainerFactory();
+		final String address = containerFactory.getAddress() == null ? "0.0.0.0" : containerFactory.getAddress().getHostAddress();
+    	containerFactory.addBuilderCustomizers(new UndertowBuilderCustomizer() {
+			@Override
+			public void customize(Builder builder) {
+				builder.addHttpListener(8080, address);
+			}
+    	});
+    	return containerFactory;
     }
 
 }
